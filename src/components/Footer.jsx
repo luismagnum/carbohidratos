bf4d8643a8d0fc91072fdb00910972cc7ad77785
luismagnum <luismagnum1@gmail.com>
2024-05-16 23:36:06 -0300

    datos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='text-center mx-auto'>
         <h2 className='text-lg text-green-500 mt-6 mb-12 font-bold'>(1 unidad por cada 10grs.)</h2>
-        <h4>Software Developer Luis E. Diaz</h4>2024
+        <h4 className='text-black font-semibold'>Software Developer Luis E. Diaz</h4>2024
     </div>
   )
 }
